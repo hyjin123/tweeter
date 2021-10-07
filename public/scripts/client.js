0a229ada3de88d2ab4d5d3be133a5b46cd6c9140
Sean Jin <seanhoyeonjin@gmail.com>
2021-10-07 01:28:44 +0000

    Added a feature to render tweets dynamically, making form submissions using jQuery, fetching tweets with Ajax, form validation, refetching tweets, cross-site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,58 +5,55 @@
  */
 $(document).ready(function() {
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+const loadTweets = function() {
+  $.ajax({
+    url: "/tweets",
+    type: "GET",
+    dataType: "JSON",
+    success: (data) => {
+      console.log("success GET REQUEST")
+      renderTweets(data);
+    }
+  })
+}
+// Load the previous made tweets as soon as the browser is loaded
+loadTweets();
+
+// preventing XSS with Escaping
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 const renderTweets = function(tweets) {
-// loops through tweets
-for (const item of tweets) {
+  // clear the previous tweets before submiting and displaying
+  $('#tweets-container').empty(); 
+  // loops through tweets
+  for (const item of tweets) {
   // calls createTweetElement for each tweet
   const $tweet = createTweetElement(item);
   // takes return value and appends it to the tweets container
-  $('#tweets-container').append($tweet);
-}
+  $('#tweets-container').prepend($tweet);
+  }
 };
 
 const createTweetElement = function (data) {
-  let markup = `
+  let $markup = `
     <article class="tweet">
       <header class="tweet-header">
         <div class="avatar-name">
-          <img class="article-avatar" src=${data.user.avatars}>
-          <span id="tweet-name" class="smaller-font">${data.user.name}</span>
+          <img class="article-avatar" src=${escape(data.user.avatars)}>
+          <span id="tweet-name" class="smaller-font">${escape(data.user.name)}</span>
         </div>
         <div id="tweet-handle">
-          <span>${data.user.handle}</span>
+          <span>${escape(data.user.handle)}</span>
         </div>
       </header>
-      <p class="smaller-font">${data.content.text}</p>
+      <p class="smaller-font">${escape(data.content.text)}</p>
       <footer class="tweet-footer">
         <div>
-          <span>${timeago.format(data.created_at)}</span>
+          <span>${escape(timeago.format(data.created_at))}</span>
         </div>
         <div>
           <i class="fas fa-flag"></i>
@@ -66,9 +63,37 @@ const createTweetElement = function (data) {
       </footer>
     </article>
    `
-  return markup;
+  return $markup;
 };
 
-renderTweets(data);
+const $form = $("form");
+$form.submit(function(event) {
+  event.preventDefault();
+  // fetch the value of the submitted tweet (data)
+  const value = $(this).serializeArray()[0].value;
+  // if the text has no length, alert a message
+  if (!value.length || value === null) {
+    $("#empty").slideDown("slow", function(){});
+  }
+  // if the text exceeds 140 characters, alert a message
+  if (value.length > 140) {
+    $("#exceed").slideDown("slow", function(){});
+  }
+  const serializedData = $(this).serialize();
+  // GET request using jQuery library
+  $.ajax({
+    url: "/tweets",
+    type: "POST",
+    dataType: "text",
+    data: serializedData,
+    success: (data) => {
+      console.log("server received the data!")
+      // Load the tweet right after submission
+      loadTweets();
+      //clear the form after data is passed to the server
+      $(this)[0].reset();
+    }
+  })
+})
 
 });
