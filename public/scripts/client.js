a0b668a80d56f3df4e4b5a2d240da415513ebf97
Sean Jin <seanhoyeonjin@gmail.com>
2021-10-08 02:29:07 +0000

    Added a second toggle button feature that appears in the right hand corner and when user clicks, scrolls to the top

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,6 +27,23 @@ $("#tweet").click(function() {
   $("textarea").focus();
 });
 
+// feature that shows the toggle button based on the vertical position
+$(document).scroll(function() {
+  let position = $(this).scrollTop();
+  if (position > 400) {
+    $("#toggle-button").fadeIn();
+  } else {
+    $("#toggle-button").fadeOut();
+  }
+});
+
+// feature that lets you scroll to the top when you toggle the button
+$("#toggle-button").click(function() {
+  $(".new-tweet").slideDown("slow");
+  $("textarea").focus();
+  window.scrollTo({top: 0, behavior: 'smooth'});
+});
+
 // preventing XSS with Escaping
 const escape = function (str) {
   let div = document.createElement("div");
